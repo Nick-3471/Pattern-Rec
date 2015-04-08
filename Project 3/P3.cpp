2d5a8ee4ade11c8ebbb25bf6dbb64ce2535f5714
Nick <nick_3471@yahoo.com>
2015-04-08 14:43:44 -0700

    Project 3

---
+++ Project 3/P3.cpp
@@ -0,0 +1,52 @@
+///////////////////////////
+//Project 3
+//Brian Goga & Nicholas Smith
+//CS 479
+//////////////////////////
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <list>
+#include <vector>
+#include <dirent.h>
+
+using namespace std;
+
+vector< vector<int> > faces;
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
